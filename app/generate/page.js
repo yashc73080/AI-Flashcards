824e99346f2dc1d5c4206da70f14f9ce8711f0ee
patelpratyush <patelpratyush28@gmail.com>
2024-08-21 13:05:00 -0400

    update

--- app/generate/page.js
+++ app/generate/page.js
@@ -104,7 +104,7 @@ const GeneratePage = () => {
                     }}
                     InputProps={{
                         style: {
-                            color: 'white',
+                            color: 'black',
                         },
                     }}
                 />
--- app/landingpage/page.js
+++ app/landingpage/page.js
@@ -47,7 +47,7 @@ const LandingPage = () => {
                 <Typography variant="h5" component="h2" gutterBottom>
                     The easiest way to create flashcards from your text.
                 </Typography>
-                    <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/generate">
+                    <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/sign-in">
                     Get Started
                 </Button>
                 <Button variant="outlined" color="primary" sx={{mt: 2}}>
@@ -59,14 +59,81 @@ const LandingPage = () => {
                 <Typography variant="h4" component="h2" gutterBottom>Features</Typography>
                 <Grid container spacing={4}>
                     {/* Feature items */}
+                    <Grid item xs={12} md={4}>
+                        <Typography variant="h6" fontWeight={600}>
+                        Easy to Use
+                        </Typography>
+                        <Typography>Add your text and we do the rest!</Typography>
+                    </Grid>
+
+                    {/* Accessible */}
+                    <Grid item xs={12} md={4}>
+                        <Typography variant="h6" fontWeight={600}>
+                        Accessible
+                        </Typography>
+                        <Typography>
+                        Access your flashcards from anywhere! Make learning on the go easy
+                        and efficient.
+                        </Typography>
+                    </Grid>
+                    {/* Organized  */}
+                    <Grid item xs={12} md={4}>
+                        <Typography variant="h6" fontWeight={600}>
+                        Organization
+                        </Typography>
+                        <Typography>
+                        Our flashcards enables users to manage multiple flashcard decks
+                        effortlessly.
+                        </Typography>
+                    </Grid>
                 </Grid>
             </Box>
 
-            <Box sx={{my: 6, textAlign: 'center'}}>
-                <Typography variant="h4" component="h2" gutterBottom>Pricing</Typography>
-                <Grid container spacing={4} justifyContent="center">
-                    {/* Pricing plans */}
-                </Grid>
+            <Box sx={{ my: 6, textAlign: "center" }}>
+            <Typography
+            variant="h4"
+            gutterBottom
+            sx={{ textDecoration: "underline" }}
+            >
+            Pricing
+            </Typography>
+            <Grid container spacing={4} justifyContent="center">
+            {/* Pricing plans */}
+            {/* Free */}
+            <Grid item xs={12} md={4}>
+                <Typography variant="h6" fontWeight={600}>
+                Free
+                </Typography>
+                <Typography>$ 0</Typography>
+                <Typography>100 flashcards</Typography>
+                <Typography>
+                No access to advanced customization or analytics
+                </Typography>
+            </Grid>
+
+            {/* Basic */}
+            <Grid item xs={12} md={4}>
+                <Typography variant="h6" fontWeight={600}>
+                Basic
+                </Typography>
+                <Typography>$ 5 / month</Typography>
+                <Typography>500 flashcards</Typography>
+                <Typography>Basic customization and analytics</Typography>
+            </Grid>
+
+            {/* Pro */}
+
+            <Grid item xs={12} md={4}>
+                <Typography variant="h6" fontWeight={600}>
+                Pro
+                </Typography>
+                <Typography>$ 10 / month</Typography>
+                <Typography>Unlimited Flashcards</Typography>
+                <Typography>
+                Advanced customization and detailed analytics
+                </Typography>
+            </Grid>
+            </Grid>
             </Box>
 
         </Box>
